53a2d121b89ce549fc7f663a1d9950bba23cb409
yasirurandula84-coder <yasirurandula84@gmail.com>
2026-03-26 19:25:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "X4IRADxA#c9Ha6aQk6g5IhofeAUIq6XVZ45LeTtWkJPFjka2K8YA",
+SESSION_ID: process.env.SESSION_ID || "mgJnxb5B#dj3obc0WGIQClT9cNFNJ4XUsq7wfihYjZZgreFI0f8s",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/ZRXhhYxH/db1c9ed7-6513-49da-8105-f21c73583135.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 VERTEX-MD Is Alive Now😍*",
 BOT_OWNER: '94704421963',  // Replace with the owner's phone number
