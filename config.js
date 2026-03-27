3fbc6b85b2270557d13ff6261d367aea1282e2dd
yasirurandula84-coder <yasirurandula84@gmail.com>
2026-03-27 11:43:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,6 +9,8 @@ SESSION_ID: process.env.SESSION_ID || "mgJnxb5B#dj3obc0WGIQClT9cNFNJ4XUsq7wfihYj
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/ZRXhhYxH/db1c9ed7-6513-49da-8105-f21c73583135.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 VERTEX-MD Is Alive Now😍*",
 BOT_OWNER: '94704421963',  // Replace with the owner's phone number
+AUTO_STATUS_SEEN: 'true',
+AUTO_STATUS_REACT: 'true',
 
 
 
